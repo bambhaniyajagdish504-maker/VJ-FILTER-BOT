cdd8c0ad0735b74809c8a6eb8d5ce44640e6e3e2
bambhaniyajagdish504-maker <bambhaniyajagdish504@gmail.com>
2025-12-16 22:23:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', '0')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+knjM74efbE1hZGNl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MoviesGroupG3')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+gVTXekxU98owZGY1') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/bambhaniya_jagdish_79')
